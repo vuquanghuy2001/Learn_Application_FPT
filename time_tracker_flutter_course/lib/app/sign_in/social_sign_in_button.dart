f2a62ed9bb09bc5aff4b11002c57a1014ad94be1
Vũ Quang Huy <vuquanghuyxl1234@gmail.com>
2021-09-05 15:27:40 +0700

    kết nối firebase

---
+++ time_tracker_flutter_course/lib/app/sign_in/social_sign_in_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
+
+class SocialSignInButton extends CustomRaisedButton {
+  SocialSignInButton({
+    required String assetName,
+    required String text,
+    required Color color,
+    required Color textColor,
+    required VoidCallback onPressed,
+  })  : assert(assetName != null),
+        assert(text != null),
+        super(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(assetName),
+              Text(
+                text,
+                style: TextStyle(color: textColor, fontSize: 15.0),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Image.asset(assetName),
+              ),
+            ],
+          ),
+          color: color,
+          height: 40.0,
+          onPressed: onPressed,
+        );
+}
